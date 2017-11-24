16df19fb4b050d4e1488dc2f77567ee59d2fdbab
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-24 13:57:35 +0600

    removed calc logic workflow

--- src/main/java/kz/epam/tam/module3/calculator/runner/Runner.java
+++ src/main/java/kz/epam/tam/module3/calculator/runner/Runner.java
@@ -1,44 +1,12 @@
 package kz.epam.tam.module3.calculator.runner;
 
-import kz.epam.tam.module3.calculator.core.Calculator;
-import kz.epam.tam.module3.calculator.utils.*;
+import kz.epam.tam.module3.calculator.workflow.CalcManager;
 
 public class Runner {
 
     public static void main(String[] args) {
-        boolean calculation = true;
-        int proceedOrExit = 1;
-        Logger.printCalcStatus(proceedOrExit);
-        while (calculation) {
-            Logger.printProceedMenu();
-            proceedOrExit = Reader.readData();
-            if (proceedOrExit == 1) {
-                Logger.printMainMenu();
-                int operationType =  Reader.readData();
-                switch (operationType) {
-                    case 1:
-                        CalcHelper.executeCalcOperation(operationType);
-                        break;
-                    case 2:
-                        CalcHelper.executeCalcOperation(operationType);
-                        break;
-                    case 3:
-                        CalcHelper.executeCalcOperation(operationType);
-                        break;
-                    case 4:
-                        CalcHelper.executeCalcOperation(operationType);
-                        break;
-                    default:
-                        Logger.printError();
-                        break;
-                }
-            } else if (proceedOrExit == 2) {
-                calculation = false;
-                Logger.printCalcStatus(proceedOrExit);
-            } else {
-                Logger.printError();
-            }
-        }
+        CalcManager calc = new CalcManager();
+        calc.executeCalculator();
     }
 
 }
